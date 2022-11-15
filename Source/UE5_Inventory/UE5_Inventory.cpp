575e5bb2d75452ab1fda999b0d770a330c789b45
noubuddy <anton.nich00@gmail.com>
2022-11-15 15:47:59 +0200

    init commit

---
+++ Source/UE5_Inventory/UE5_Inventory.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_Inventory.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_Inventory, "UE5_Inventory" );
+
+DEFINE_LOG_CATEGORY(LogUE5_Inventory)
+ 
